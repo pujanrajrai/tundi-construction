d68013ac2544f5287ec1677749a14a2448cc5f03
pujanrajrai <pujanr0809@gmail.com>
2021-09-28 16:11:38 +0545

    add requirements.txt

--- tundi_construction_production/settings.py
+++ tundi_construction_production/settings.py
@@ -129,8 +129,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
